2533a870b2b448cc05105a996e4affb1c07f3e11
Dessie <desislava.m.alexandrova@gmail.com>
2025-06-06 19:13:49 +0300

    Creating different pages

---
+++ src/pages/Book.jsx
@@ -0,0 +1,5 @@
+export default function Book() {
+    return (
+        <h1>Boook</h1>
+    )
+}
---
+++ src/pages/BookList.jsx
@@ -0,0 +1,5 @@
+export default function BookList() {
+    return (
+        <h1>Book List</h1>
+    )
+}
